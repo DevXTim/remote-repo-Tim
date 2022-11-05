f9061010a3e1a14d4abeea15abf1d13d9d21477d
DevXTim <temirlan.blank@gmail.com>
2022-11-05 07:50:26 +0600

    new commit

--- src/RemoteApp.java
+++ src/RemoteApp.java
@@ -1,5 +1,7 @@
 public class RemoteApp {
     public static void main(String[] args) {
         System.out.println("Remote repo message from Tim app");
+
+        System.out.println("Print message from second commit");
     }
 }
